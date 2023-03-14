5ae04c602d4c2a63eba26a6714d74bef95bc0934
Rohit Singh <r.singh789583@gmail.com.com>
2023-03-14 17:41:33 +0530

    add about section

--- script.js
+++ script.js
@@ -2,3 +2,18 @@ window.addEventListener("scroll", function(){
     var nav = document.querySelector("nav");
     nav.classList.toggle("sticky", window.scrollY > 0);
 })
+
+const menu = document.querySelector(".menu");
+const navItems = document.querySelector('.navItems');
+const fullBody = document.querySelector('.fullbody');
+menu.onclick = () => {
+    menu.classList.toggle('active')
+    navItems.classList.toggle('active')
+    fullBody.classList.toggle('active')
+}
+fullBody.onclick = (e) => {
+    if(menu.contains(e.target) && !navItems.contains(e.target))
+    menu.classList.remove('active')
+    navItems.classList.remove('active')
+    fullBody.classList.remove('active')
+}
